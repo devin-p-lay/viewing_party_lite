9ce306d6d214e778c1df6adeef998da46ae1e13a
Devin Pile <devinpile@gmail.com>
2022-02-01 02:50:11 -0700

    Add sad path tests for register new user

--- spec/features/users/register_spec.rb
+++ spec/features/users/register_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe 'register user' do
   before do
+    @user2 = User.create(name:'Eric', email:'eric@faker.net')
     visit '/register'
   end
 
@@ -12,6 +13,23 @@ describe 'register user' do
       click_on 'Register'
       new_user = User.last
       expect(current_path).to eq(user_dashboard_path(new_user))
+      expect(page).to have_content("Welcome to the Viewing Party!")
     end
+
+    it 'cannot add because of duplicate email' do
+      fill_in 'Name', with: 'Milk Man'
+      fill_in 'Email', with: 'eric@faker.net'
+      click_on 'Register'
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content("Unable to Register New User")
+    end
+
+    it 'cannot add because form is filled out incorrectly' do
+      fill_in 'Name', with: ''
+      fill_in 'Email', with: ''
+      click_on 'Register'
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content("Unable to Register New User")
+    end 
   end
 end
