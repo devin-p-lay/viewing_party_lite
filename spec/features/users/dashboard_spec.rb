1e01ad3427ac2d9a45893faaa3ccab116c681b0f
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-01 16:18:06 -0700

    Create test for users parties display

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'users dashboard' do
   before :each do
     @user_1 = User.create!(name: "Bob Barker", email: "BobBarker@example.com")
     @user_2 = User.create!(name: "Jeff Lebowski", email: "TheDude@example.com")
+    @user_3 = User.create!(name: "Eyore", email: "NoFriends@example.com")
   end
 
   it 'dsplays the users name at the top of the page' do
@@ -33,6 +34,39 @@ RSpec.describe 'users dashboard' do
   end
 
   it 'provides a section where any viewing parties are listed' do
+    party_1 = Party.create!(date: Time.current, start_time: Time.current, movie_title: "Big Lebowski", movie_runtime: "12345")
+    Attendee.create!(user: @user_1, party: party_1, role: 1)
+    Attendee.create!(user: @user_2, party: party_1, role: 0)
 
+    visit user_dashboard_path(@user_1)
+
+    expect(page).to have_content("It's Party Time!")
+
+    within("#party_time_#{party_1.id}") do
+      expect(page).to have_content("Movie: Big Lebowski")
+      expect(page).to have_content("Runtime: 12345")
+      expect(page).to have_content("Date: #{party_1.date}")
+      expect(page).to have_content("Start time: #{party_1.start_time}")
+    end
+
+    visit user_dashboard_path(@user_2)
+
+    expect(page).to have_content("It's Party Time!")
+
+    within("#party_time_#{party_1.id}") do
+      expect(page).to have_content("Movie: Big Lebowski")
+      expect(page).to have_content("Runtime: 12345")
+      expect(page).to have_content("Date: #{party_1.date}")
+      expect(page).to have_content("Start time: #{party_1.start_time}")
+    end
+
+    visit user_dashboard_path(@user_3)
+
+    expect(page).to have_content("It's Party Time!")
+
+    expect(page).to_not have_content("Movie: Big Lebowski")
+    expect(page).to_not have_content("Runtime: 12345")
+    expect(page).to_not have_content("Date: #{party_1.date}")
+    expect(page).to_not have_content("Start time: #{party_1.start_time}")
   end
 end
