d71fb70e3090dec5c7ad04f119df8c772f588df8
Devin Pile <devinpile@gmail.com>
2022-02-01 12:16:25 -0700

    Add changes to implement register new user

--- spec/features/users/register_spec.rb
+++ spec/features/users/register_spec.rb
@@ -8,28 +8,28 @@ describe 'register user' do
 
   describe 'form to register' do
     it 'adds a new user' do
-      fill_in 'Name', with: 'Elora'
-      fill_in 'Email', with: 'elora@faker.net'
-      click_on 'Register'
+      fill_in :name, with: 'Elora'
+      fill_in :email, with: 'elora@faker.net'
+      click_on 'Create New User'
       new_user = User.last
-      expect(current_path).to eq(user_dashboard_path(new_user))
-      expect(page).to have_content("Welcome to the Viewing Party!")
+      expect(current_path).to eq("/users/#{new_user.id}/dashboard")
+      expect(page).to have_content("Account Successfully Created!")
     end
 
     it 'cannot add because of duplicate email' do
-      fill_in 'Name', with: 'Milk Man'
-      fill_in 'Email', with: 'eric@faker.net'
-      click_on 'Register'
+      fill_in :name, with: 'Milk Man'
+      fill_in :email, with: 'eric@faker.net'
+      click_on 'Create New User'
       expect(current_path).to eq(register_path)
-      expect(page).to have_content("Unable to Register New User")
+      expect(page).to have_content('Unable to register, please try again.')
     end
 
     it 'cannot add because form is filled out incorrectly' do
-      fill_in 'Name', with: ''
-      fill_in 'Email', with: ''
-      click_on 'Register'
+      fill_in :name, with: ''
+      fill_in :email, with: ''
+      click_on 'Create New User'
       expect(current_path).to eq(register_path)
-      expect(page).to have_content("Unable to Register New User")
-    end 
+      expect(page).to have_content('Unable to register, please try again.')
+    end
   end
 end
