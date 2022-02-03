52d6d6051f8dd3fb07cb465ea1e19cd19a41bb81
Devin Pile <devinpile@gmail.com>
2022-02-02 20:03:39 -0700

    Add require faraday and json to application.rb

---
+++ app/facades/movies_facade.rb
@@ -0,0 +1,17 @@
+class MovieFacade
+  class << self
+    def top_40_movies
+      data = MovieService.top_movies(1).concat(MovieService.top_movies(2))
+      data.map do |movie_data|
+        Movie.new(movie_data)
+      end
+    end
+
+    def movie_search(search_param)
+      data = MovieService.search_movies(search_param, 1).concat(MovieService.search_movies(search_param, 2))
+      data.map do |movie_data|
+        Movie.new(movie_data)
+      end
+    end
+  end
+end
