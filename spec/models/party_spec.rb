fd7168b6e4d7a9cca2773c5b8e27cb00c6a0444c
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-04 11:26:05 -0700

    Create test for #whos_hosting

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -12,4 +12,15 @@ describe Party do
     it { should validate_presence_of(:movie_id) }
     it { should validate_presence_of(:length) }
   end
+  
+  describe 'instance methods' do
+    context '#whos_hosting?' do
+      it 'can find a user whos id matches the host id' do
+        user_1 = User.create!(name: "Bob Barker", email: "BobBarker@example.com")
+        party_1 = Party.create!(host_id: user_1.id, movie_id: 278, length: 142, start_time: "19:00:00", date:  "2022-04-03")
+
+          expect(party_1.whos_hosting?).to eq("Bob Barker")
+      end
+    end
+  end
 end
